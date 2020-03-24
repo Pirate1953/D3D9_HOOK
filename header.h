c3ac7b2dff39fb3311594fe9ba3b7cbe10377e7a
Pirate1953 <piratefromnev1953@gmail.com>
2020-03-24 15:37:25 +0300

    d3d9_hook_codeInject

--- header.h
+++ header.h
@@ -2,136 +2,56 @@
 #ifndef HEADER_H
 #define HEADER_H
 
-#pragma comment (lib, "d3dx9.lib")
-#pragma comment (lib, "d3d9.lib")
-
-#include <Windows.h>
 #include <d3d9.h>
-#include <d3dx9.h>
+#include <d3dx9core.h>
+
+#include <vector>
 #include <iostream>
 #include <fstream>
-#include <detours.h>
 
-int fontSize = 20;
+#pragma comment(lib, "d3dx9.lib")
+#pragma comment(lib, "d3d9.lib")
 
-int MenuX = 0;
-int MenuY = 0;
-
-int MenuW = 165;
-
-bool isMenuOn = true;
-
-typedef HRESULT(WINAPI *tSetStreamSource)(LPDIRECT3DDEVICE9 pDevice, UINT StreamNumber, IDirect3DVertexBuffer9 *pStreamData, UINT OffsetInBytes, UINT Stride);
-tSetStreamSource oSetStreamSource;
+//typedef IDirect3D9* (WINAPI *DIRECTCREATE9)(unsigned int); //Pointer to D3D Device
 
 typedef HRESULT(WINAPI *tEndScene)(LPDIRECT3DDEVICE9 pDevice);
-tEndScene oEndScene;
-
 typedef HRESULT(WINAPI *tDrawIndexedPrimitive)(LPDIRECT3DDEVICE9 pDevice, D3DPRIMITIVETYPE PrimType, INT BaseVertexIndex, UINT MinVertexIndex, UINT NumVertices, UINT startIndex, UINT primCount);
+
 tDrawIndexedPrimitive oDrawIndexedPrimitive;
+tEndScene oEndScene;
+bool StartLog = false;
+D3DVIEWPORT9 Vpt;
+LPDIRECT3DTEXTURE9 Green = NULL;
+LPDIRECT3DTEXTURE9 Red = NULL;
+LPDIRECT3DTEXTURE9 pTx = NULL;
+D3DLOCKED_RECT d3dlr;
+LPD3DXFONT pFont = NULL;
+char strbuff[250];
 
-unsigned int uiStride = NULL;
+BYTE codeFragment_endscene[5] = { 0, 0, 0, 0, 0 };
+BYTE jump_endscene[5] = { 0, 0, 0, 0, 0 };
+DWORD savedProtection_endscene = 0;
 
-LPDIRECT3DDEVICE9 g_pDevice = NULL;
-LPD3DXLINE g_Line = NULL;
-LPD3DXFONT g_pFont = NULL;
+BYTE codeFragment_drawindex[5] = { 0, 0, 0, 0, 0 };
+BYTE jump_drawindex[5] = { 0, 0, 0, 0, 0 };
+DWORD savedProtection_drawindex = 0;
 
-LPDIRECT3DTEXTURE9 g_pTexRed = NULL;
-LPDIRECT3DTEXTURE9 g_pTexBlue = NULL;
-LPDIRECT3DTEXTURE9 g_pTexGreen = NULL;
+//Methods from vTable
+DWORD endsc = 0;
+DWORD drawindex = 0;
 
-D3DVIEWPORT9 g_ViewPort;
+//For logging
+UINT iStride = 0;
+UINT iBaseTex = 0;
+LPDIRECT3DBASETEXTURE9 BTEX = NULL;
+bool Found;
+std::vector<DWORD> BASETEX;
 
-int selector = 0;
-int itemSelector = -1;
-
-int MenuH = 0;
-
-bool isFirstRun = true;
-
-#define HOOK(func, addy) o##func = (t##func)DetourFunction((PBYTE)addy, (PBYTE)hk##func)
-
-#define ES 0
-#define DIP	1
-#define SSS	2
-
-#define RED D3DCOLOR_ARGB(255, 255, 0, 0)
-#define GREEN D3DCOLOR_ARGB(255, 0, 255, 0)
-#define BLUE D3DCOLOR_ARGB(255, 0, 0, 255)
-#define WHITE D3DCOLOR_ARGB(255, 255, 255, 255)
-#define BLACKTransparent D3DCOLOR_ARGB(220, 0, 0, 0)
-#define BLACK D3DCOLOR_ARGB(255, 0, 0, 0)
-#define YELLOW D3DCOLOR_ARGB(255, 255, 255, 0)
-#define TEAL D3DCOLOR_ARGB(255, 0, 255, 255)
-#define PINK D3DCOLOR_ARGB(255, 255, 240, 0)
-#define ORANGE D3DCOLOR_ARGB(255, 255, 132, 0)
-#define LIME D3DCOLOR_ARGB(255, 198, 255, 0)
-#define SKYBLUE D3DCOLOR_ARGB(255, 0, 180, 255)
-#define MAROON D3DCOLOR_ARGB(255, 142, 30, 0)
-#define LGRAY D3DCOLOR_ARGB(255, 174, 174, 174) 
-#define DGRAY D3DCOLOR_ARGB(255, 71, 65, 64) 
-#define BROWN D3DCOLOR_ARGB(255, 77, 46, 38)
-#define SHIT D3DCOLOR_ARGB(255, 74, 38, 38)
-
-const BYTE bRed[58] = {
-	0x42, 0x4D, 0x3A, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x36, 0x00, 0x00, 0x00, 0x28, 0x00,
-	0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x01, 0x00, 0x18, 0x00, 0x00, 0x00,
-	0x00, 0x00, 0x04, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xFF, 0x00
-};
-
-const BYTE bGreen[60] = {
-	0x42, 0x4D, 0x3C, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-	0x00, 0x36, 0x00, 0x00, 0x00, 0x28, 0x00, 0x00, 0x00,
-	0x01, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x01,
-	0x00, 0x20, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-	0x00, 0x00, 0x12, 0x0B, 0x00, 0x00, 0x12, 0x0B, 0x00,
-	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-	0x20, 0xA0, 0x00, 0x00, 0xFF, 0xFF
-};
-
-const BYTE bBlue[60] = {
-	0x42, 0x4D, 0x3C, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-	0x00, 0x36, 0x00, 0x00, 0x00, 0x28, 0x00, 0x00, 0x00,
-	0x01, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x01,
-	0x00, 0x20, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-	0x00, 0x00, 0x12, 0x0B, 0x00, 0x00, 0x12, 0x0B, 0x00,
-	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-	0xFF, 0x00, 0x00, 0x00, 0x00, 0x00
-};
-
-struct HACK
-{
-	int id;
-	int value = 0;
-	int type;
-	char *Name;
-	int maxVal = 0;
-};
-
-struct Colum
-{
-	int y = 0;
-	bool on = false;
-	int hacks = 0;
-	char *Name;
-	HACK hk[15];
-};
-
-class TpMenu
-{
-public:
-	int colCount = 0;
-	int selector = 0;
-
-	Colum Col[10];
-	HACK Hack[15];
-
-	void AddColum(int id, char *name);
-	void AddItem(int colum, int id, char *name, int type);
-	void BuildMenu(char *name, int x, int y, int w);
-	void KeyboardEvents();
-	void StartHack();
-};
+DWORD btex = 0;
+UINT BaseVertexIndexFound = 0;
+UINT MinVertexIndexFound = 0;
+UINT NumVerticesFound = 0;
+UINT startIndexFound = 0;
+UINT PrimitiveCountFound = 0;
 
 #endif
--- main.cpp
+++ main.cpp
@@ -1,352 +1,202 @@
 #include "header.h"
 
-using namespace std;
-
-void PrintText(char pString[], int x, int y, D3DCOLOR col, ID3DXFont *font)
+//Get Virtual Methods Table
+void GetDevice9Methods()
 {
-	RECT FontRect = { x, y, x + 500, y + 30 };
-	font->DrawText(NULL, pString, -1, &FontRect, DT_LEFT | DT_WORDBREAK, col);
-}
-
-int GetTextWidth(const char *szText, ID3DXFont *pFont)
-{
-	RECT rcRect = { 0, 0, 0, 0 };
-	if (pFont)
-	{
-		pFont->DrawText(NULL, szText, (int)strlen(szText), &rcRect, DT_CALCRECT, D3DCOLOR_XRGB(0, 0, 0));
-	}
-	return rcRect.right - rcRect.left;
-}
-
-void FillRGB(int x, int y, int w, int h, D3DCOLOR color, IDirect3DDevice9 *pDevice)
-{
-	if (w < 0) w = 1;
-	if (h < 0) h = 1;
-	if (x < 0) x = 1;
-	if (y < 0) y = 1;
-
-	D3DRECT rec = { x, y, x + w, y + h };
-	pDevice->Clear(1, &rec, D3DCLEAR_TARGET, color, 0, 0);
-}
-
-void FillARGB(int x, int y, int w, int h, D3DCOLOR color)
-{
-	D3DXVECTOR2 DotPosition[2];
-	DotPosition[0] = D3DXVECTOR2((float)(x + w / 2), (float)y);
-	DotPosition[1] = D3DXVECTOR2((float)(x + w / 2), (float)(y + h));
-
-	g_Line->SetAntialias(false);
-	g_Line->SetWidth((float)w);
-	g_Line->Draw(DotPosition, 2, color);
-}
-
-void DrawBorder(int x, int y, int w, int h, int px, D3DCOLOR BorderColor, IDirect3DDevice9 *pDevice)
-{
-	FillRGB(x, (y + h - px), w, px, BorderColor, pDevice);
-	FillRGB(x, y, px, h, BorderColor, pDevice);
-	FillRGB(x, y, w, px, BorderColor, pDevice);
-	FillRGB((x + w - px), y, px, h, BorderColor, pDevice);
-}
-
-void DrawBoxArgb(int x, int y, int w, int h, D3DCOLOR BoxColor, D3DCOLOR BorderColor, IDirect3DDevice9* pDevice)
-{
-	FillARGB(x, y, w, h, BoxColor);
-	DrawBorder(x, y, w, h, 1, BorderColor, pDevice);
-}
-
-void DrawBox(int x, int y, int w, int h, D3DCOLOR BoxColor, D3DCOLOR BorderColor, IDirect3DDevice9* pDevice)
-{
-	FillRGB(x, y, w, h, BoxColor, pDevice);
-	DrawBorder(x, y, w, h, 1, BorderColor, pDevice);
-}
-
-TpMenu menu;
-
-void TpMenu::StartHack()
-{
-	if (Col[2].hk[0].value == 1)
-	{
-		FillRGB(g_ViewPort.Width / 2 - 10, g_ViewPort.Height / 2, 20, 1, RED, g_pDevice);
-		FillRGB(g_ViewPort.Width / 2, g_ViewPort.Height / 2 - 10, 1, 20, RED, g_pDevice);
-	}
-}
-
-void TpMenu::AddColum(int id, char *name)
-{
-	Col[id].Name = name;
-	colCount++;
-}
-
-void TpMenu::AddItem(int colum, int id, char *name, int type)
-{
-	Col[colum].hk[id].id = id;
-	Col[colum].hk[id].Name = name;
-	Col[colum].hk[id].type = type;
-	switch (type)
-	{
-	case 0:
-		Col[colum].hk[id].maxVal = 1;
-		break;
-	case 1:
-		Col[colum].hk[id].maxVal = 99;
-		break;
-	case 2:
-		Col[colum].hk[id].maxVal = 4;
-		break;
-	}
-	Col[colum].hacks++;
-}
-
-void TpMenu::BuildMenu(char *name, int x, int y, int w)
-{
-	if (isMenuOn)
-	{
-		FillARGB(x, y, w, fontSize + 5, BLUE);
-		DrawBorder(x, y, w, fontSize + 5, 1, RED, g_pDevice);
-
-		PrintText(name, x + (w - GetTextWidth(name, g_pFont)) / 2, y + 2, GREEN, g_pFont);
-
-		FillARGB(x + 3, y + fontSize + 5, MenuW - 6, MenuH - (fontSize + 5), BLUE);
-		DrawBorder(x + 3, y + fontSize + 5, MenuW - 6, MenuH - (fontSize + 5), 1, RED, g_pDevice);
-
-
-		MenuH = fontSize + 10;
-
-
-		for (int i = 0; i < colCount; i++)
-		{
-			char hName[15];
-			sprintf_s(hName, "[%s]", Col[i].Name);
-
-			DrawBorder(x + 5, y + MenuH, w - 10, fontSize + 10, 1, (selector == i && itemSelector == -1) ? GREEN : RED, g_pDevice);
-			PrintText(hName, x + (w - 10 - GetTextWidth(hName, g_pFont)) / 2, y + MenuH + 5, Col[i].on ? GREEN : RED, g_pFont);
-			Col[i].y = y + MenuH;
-			MenuH += fontSize + 13;
-			if (Col[i].on)
-				for (int a = 0; a < Col[i].hacks; a++)
-				{
-					PrintText(Col[i].hk[a].Name, x + 10, y + MenuH, selector == i && itemSelector == a ? GREEN : RED, g_pFont);	
-					PrintText(Col[i].hk[a].value == 0 ? "N" : "Y", x + w - 35, y + MenuH, Col[i].hk[a].value == 0 ? RED : GREEN, g_pFont);
-					MenuH += fontSize + 3;
-
-				}
-
-		}
-		FillRGB(x + 3, y + MenuH + 3, w - 6, 3, GREEN, g_pDevice);
-		PrintText("This is footer!", x + (w - GetTextWidth("This is footer!", g_pFont)) / 2, y + MenuH + 6, GREEN, g_pFont);
-		MenuH += fontSize + 8;
-	}
-}
-
-void TpMenu::KeyboardEvents()
-{
-	if (GetAsyncKeyState(VK_INSERT) & 1)
-		isMenuOn = !isMenuOn;
-
-	if (GetAsyncKeyState(VK_UP) & 1)
-	{
-		if (selector > 0 && !Col[selector].on)
-		{
-			selector--;
-			if (Col[selector].on)
-			{
-				itemSelector = Col[selector].hacks - 1;
-				return;
-			}
-		}
-
-		if (Col[selector].on && itemSelector > -1)  itemSelector--;
-		else
-		{
-			if (selector > 0)
-			{
-
-				if (Col[selector].on)
-				{
-					selector--;
-					if (Col[selector].on)
-						itemSelector = Col[selector].hacks - 1;
-				}
-				else
-					itemSelector = -1;
-
-			}
-		}
-	}
-	if (GetAsyncKeyState(VK_DOWN) & 1)
-	{
-		if (selector < colCount - 1 && !Col[selector].on)
-		{
-			selector++;
-			if (Col[selector].on)
-				itemSelector = -1;
-			return;
-		}
-
-		if (Col[selector].on && itemSelector < Col[selector].hacks - 1) itemSelector++;
-		else
-		{
-			if (selector < colCount - 1)
-			{
-				if (Col[selector].on)
-					selector++;
-				itemSelector = -1;
-			}
-		}
-	}
-	if (GetAsyncKeyState(VK_RIGHT) & 1)
-	{
-		if (itemSelector == -1)
-			Col[selector].on = true;
-		if (Col[selector].on &&  Col[selector].hk[itemSelector].value < Col[selector].hk[itemSelector].maxVal)
-			Col[selector].hk[itemSelector].value++;
-
-	}
-	if (GetAsyncKeyState(VK_LEFT) & 1)
-	{
-		if (itemSelector == -1)
-			Col[selector].on = false;
-
-		if (Col[selector].on &&  Col[selector].hk[itemSelector].value > 0)
-			Col[selector].hk[itemSelector].value--;
-	}
+	HWND hWnd = CreateWindowA("STATIC", "dummy", 0, 0, 0, 0, 0, 0, 0, 0, 0);
+	HMODULE hD3D9 = LoadLibrary("d3d9");
+	IDirect3D9* d3d = Direct3DCreate9(D3D_SDK_VERSION);
+	D3DDISPLAYMODE d3ddm;
+	d3d->GetAdapterDisplayMode(0, &d3ddm);
+	D3DPRESENT_PARAMETERS d3dpp;
+	ZeroMemory(&d3dpp, sizeof(d3dpp));
+	d3dpp.Windowed = 1;
+	d3dpp.SwapEffect = D3DSWAPEFFECT_DISCARD;
+	d3dpp.BackBufferFormat = d3ddm.Format;
+	IDirect3DDevice9* d3dDevice = 0;
+	d3d->CreateDevice(0, D3DDEVTYPE_HAL, hWnd, D3DCREATE_HARDWARE_VERTEXPROCESSING, &d3dpp, &d3dDevice);
+	DWORD* vTable = (DWORD*)(*((DWORD*)d3dDevice));
+	endsc = vTable[42] - (DWORD)hD3D9;
+	drawindex = vTable[82] - (DWORD)hD3D9;
+	d3dDevice->Release();
+	d3d->Release();
+	FreeLibrary(hD3D9);
+	CloseHandle(hWnd);
 }
 
 HRESULT WINAPI hkEndScene(LPDIRECT3DDEVICE9 pDevice)
 {
-	if (g_pDevice == NULL) g_pDevice = pDevice;
+	//Restore bytes of original func
+	BYTE* codeDest = (BYTE*)oEndScene;
+	codeDest[0] = codeFragment_endscene[0];
+	*((DWORD*)(codeDest + 1)) = *((DWORD*)(codeFragment_endscene + 1));
 
-	if (g_pTexRed == NULL)	D3DXCreateTextureFromFileInMemory(pDevice, (LPCVOID)&bRed, sizeof(bRed), &g_pTexRed);
-	if (g_pTexBlue == NULL)	D3DXCreateTextureFromFileInMemory(pDevice, (LPCVOID)&bBlue, sizeof(bBlue), &g_pTexBlue);
-	if (g_pTexGreen == NULL)	D3DXCreateTextureFromFileInMemory(pDevice, (LPCVOID)&bGreen, sizeof(bGreen), &g_pTexGreen);
+	pDevice->GetViewport(&Vpt);
+	RECT FRect = {
+		static_cast<long>(Vpt.Width - 500),
+		static_cast<long>(Vpt.Height - 500),
+		static_cast<long>(Vpt.Width),
+		static_cast<long>(Vpt.Height)
+	};
+	if (pDevice->CreateTexture(8, 8, 1, 0, D3DFMT_A8R8G8B8, D3DPOOL_DEFAULT, &Green, 0) == S_OK)
+		if (pDevice->CreateTexture(8, 8, 1, 0, D3DFMT_A8R8G8B8, D3DPOOL_SYSTEMMEM, &pTx, 0) == S_OK)
+			if (pTx->LockRect(0, &d3dlr, 0, D3DLOCK_DONOTWAIT | D3DLOCK_NOSYSLOCK) == S_OK)
+			{
+				for (UINT xy = 0; xy < 8 * 8; xy++)
+				{
+					((PDWORD)d3dlr.pBits)[xy] = 0xFF00FF00;
+				}
+				pTx->UnlockRect(0);
+				pDevice->UpdateTexture(pTx, Green);
+				pTx->Release();
+			}
 
-	if (g_pFont == NULL) D3DXCreateFont(pDevice, fontSize, 0, FW_BOLD, 1, 0, DEFAULT_CHARSET, OUT_DEFAULT_PRECIS, ANTIALIASED_QUALITY, DEFAULT_PITCH | FF_DONTCARE, "Lao UI", &g_pFont);
+	if (pFont == 0)
+		D3DXCreateFontA(pDevice, 25, 15, 700, 0, 0, 1, 0, 0, DEFAULT_PITCH | FF_DONTCARE, "Calibri", &pFont);
 
-	if (g_Line == NULL) D3DXCreateLine(pDevice, &g_Line);
+	if (pFont && StartLog)
+	{
+		sprintf_s(strbuff, "Textures: %i\nStride: %i\nBase Tex Num: %i\n" \
+			"Log Enable: %i\n\nNumPad1: Stride++ \nNumPad2: Stride-- \nNumPad4: BaseTexNum++ \n" \
+			"NumPad5: BaseTexNum-- \nNumPad0: Log On/Off \n%i %i %i %i %i %i", \
+			BASETEX.size(),
+			iStride,
+			iBaseTex + 1,
+			StartLog,
 
-	pDevice->GetViewport(&g_ViewPort);
-
-
-
-	if (g_pFont != NULL && g_Line != NULL) {
-		if (isFirstRun)
-		{
-			menu.AddColum(0, "All WH");
-
-			menu.AddItem(0, 0, "Polygons", 0);
-
-
-			isFirstRun = false;
-		}
-		menu.BuildMenu("My Menu", MenuX, MenuY, MenuW);
-		menu.KeyboardEvents();
-		menu.StartHack();
-
+			BaseVertexIndexFound,
+			MinVertexIndexFound,
+			NumVerticesFound,
+			startIndexFound,
+			PrimitiveCountFound,
+			btex);
+		pFont->DrawTextA(0, strbuff, -1, &FRect, DT_CENTER | DT_NOCLIP, 0xFF00FF00);
 	}
-	return oEndScene(pDevice);
+
+	HRESULT res = oEndScene(pDevice);	//Call original func
+
+	codeDest[0] = jump_endscene[0];
+	*((DWORD*)(codeDest + 1)) = *((DWORD*)(jump_endscene + 1));
+	
+	return res;
 }
 
 HRESULT WINAPI hkDrawIndexedPrimitive(LPDIRECT3DDEVICE9 pDevice, D3DPRIMITIVETYPE PrimType, INT BaseVertexIndex, UINT MinVertexIndex, UINT NumVertices, UINT startIndex, UINT PrimitiveCount)
 {
-	for (int i = 0; i < 10; i++)
+	BYTE* codeDest = (BYTE*)oDrawIndexedPrimitive;
+	codeDest[0] = codeFragment_drawindex[0];
+	*((DWORD*)(codeDest + 1)) = *((DWORD*)(codeFragment_drawindex + 1));
+
+	LPDIRECT3DVERTEXBUFFER9 Stream_Data;
+	UINT Offset = 0;
+	UINT Stride = 0;
+
+	if (pDevice->GetStreamSource(0, &Stream_Data, &Offset, &Stride) == S_OK)
+		Stream_Data->Release();
+
+	if (StartLog == true)
 	{
-		for (int j = 0; j < 15; j++)
+		if (Stride == iStride)
 		{
-			if (menu.Col[i].hk[j].Name == "Polygons")
+			pDevice->GetTexture(0, &BTEX);
+			Found = false;
+			for (UINT i = 0; i < BASETEX.size(); i++) {
+				if (BASETEX[i] == (DWORD)BTEX)
+					Found = true; break;
+			}
+			if (Found == false)
+				BASETEX.push_back((DWORD)BTEX);
+			if (BASETEX[iBaseTex] == (DWORD)BTEX && Green)
 			{
-				if (menu.Col[i].hk[j].value == 0)
-				{
-					pDevice->SetRenderState(D3DRS_FILLMODE, D3DFILL_SOLID);
-				}
-				else
-				{
-					pDevice->SetRenderState(D3DRS_FILLMODE, D3DFILL_WIREFRAME);
-					pDevice->SetRenderState(D3DRS_ZENABLE, D3DZB_FALSE);
-					oDrawIndexedPrimitive(pDevice, PrimType, BaseVertexIndex, MinVertexIndex, NumVertices, startIndex, PrimitiveCount);
-				}
-				break;
+				btex = (DWORD)BTEX;
+				BaseVertexIndexFound = BaseVertexIndex;
+				MinVertexIndexFound = MinVertexIndex;
+				NumVerticesFound = NumVertices;
+				startIndexFound = startIndex;
+				PrimitiveCountFound = PrimitiveCount;
+				pDevice->SetTexture(0, Green);
+				pDevice->SetRenderState(D3DRS_ZENABLE, D3DZB_FALSE);
+				oDrawIndexedPrimitive(pDevice, PrimType, BaseVertexIndex, MinVertexIndex, NumVertices, startIndex, PrimitiveCount);
+
+				pDevice->SetRenderState(D3DRS_ZENABLE, D3DZB_TRUE);
 			}
 		}
 	}
+	HRESULT res = oDrawIndexedPrimitive(pDevice, PrimType, BaseVertexIndex, MinVertexIndex, NumVertices, startIndex, PrimitiveCount);
 
-	__asm nop
-
-	HRESULT hRet = oDrawIndexedPrimitive(pDevice, PrimType, BaseVertexIndex, MinVertexIndex, NumVertices, startIndex, PrimitiveCount);
-	return hRet;
+	codeDest[0] = jump_drawindex[0];
+	*((DWORD*)(codeDest + 1)) = *((DWORD*)(jump_drawindex + 1));
+	return res;
 }
 
-HRESULT WINAPI hkSetStreamSource(LPDIRECT3DDEVICE9 pDevice, UINT StreamNumber, IDirect3DVertexBuffer9* pStreamData, UINT OffsetInBytes, UINT Stride)
+//Code Inject
+void HookDevice9Methods()
 {
-	__asm nop
+	HMODULE hD3D9 = GetModuleHandle("d3d9.dll");
 
-	if (StreamNumber == 0) {
-		uiStride = Stride;
-	}
+	oEndScene = (tEndScene)((DWORD)hD3D9 + endsc);
 
-	return oSetStreamSource(pDevice, StreamNumber, pStreamData, OffsetInBytes, Stride);
+	jump_endscene[0] = 0xE9;
+	DWORD addr_endscene = (DWORD)hkEndScene - (DWORD)oEndScene - 5;	//Address of Inject
+	memcpy(jump_endscene + 1, &addr_endscene, sizeof(DWORD));
+	memcpy(codeFragment_endscene, oEndScene, 5);	//Save code for hook of original func
+	VirtualProtect(oEndScene, 8, PAGE_EXECUTE_READWRITE, &savedProtection_endscene);
+	memcpy(oEndScene, jump_endscene, 5);	//Replace code with jump to Inject
+
+	/////////////////////////////////////////////////////////////////////////////////////
+
+	oDrawIndexedPrimitive = (tDrawIndexedPrimitive)((DWORD)hD3D9 + drawindex);
+	jump_drawindex[0] = 0xE9;
+	DWORD addr_drawindex = (DWORD)hkDrawIndexedPrimitive - (DWORD)oDrawIndexedPrimitive - 5;
+	memcpy(jump_drawindex + 1, &addr_drawindex, sizeof(DWORD));
+	memcpy(codeFragment_drawindex, oDrawIndexedPrimitive, 5);
+	VirtualProtect(oDrawIndexedPrimitive, 8, PAGE_EXECUTE_READWRITE, &savedProtection_drawindex);
+	memcpy(oDrawIndexedPrimitive, jump_drawindex, 5);
 }
 
-LRESULT CALLBACK MsgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) 
-{ 
-	return DefWindowProc(hwnd, uMsg, wParam, lParam);
-}
-
-void DX_Init(DWORD *table)
+DWORD WINAPI TF(void* lParam)
 {
-	WNDCLASSEX wc = 
-	{ 
-		sizeof(WNDCLASSEX), CS_CLASSDC, MsgProc, 0L, 0L, GetModuleHandle(NULL), NULL, NULL, NULL, NULL, "DX", NULL 
-	};
-	RegisterClassEx(&wc);
-	
-	HWND hWnd = CreateWindow("DX", NULL, WS_OVERLAPPEDWINDOW, 100, 100, 300, 300, GetDesktopWindow(), NULL, wc.hInstance, NULL);
-	
-	LPDIRECT3D9 pD3D = Direct3DCreate9(D3D_SDK_VERSION);
-	D3DPRESENT_PARAMETERS d3dpp;
-	
-	ZeroMemory(&d3dpp, sizeof(d3dpp));
-	d3dpp.Windowed = TRUE;
-	d3dpp.SwapEffect = D3DSWAPEFFECT_DISCARD;
-	d3dpp.BackBufferFormat = D3DFMT_UNKNOWN;
-
-	LPDIRECT3DDEVICE9 pd3dDevice;
-
-	pD3D->CreateDevice(D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL, hWnd, D3DCREATE_SOFTWARE_VERTEXPROCESSING, &d3dpp, &pd3dDevice);
-	
-	DWORD* pVTable = (DWORD*)pd3dDevice;
-	pVTable = (DWORD*)pVTable[0];
-
-	table[ES] = pVTable[42];
-	table[DIP] = pVTable[82];
-	table[SSS] = pVTable[100];
-
-	DestroyWindow(hWnd);
-}
-
-DWORD WINAPI MyThread(LPVOID)
-{
-	DWORD VTable[3] = { 0 };
-
-	while (GetModuleHandle("d3d9.dll") == NULL)
-	{
-		Sleep(250);
-	}
-
-	DX_Init(VTable);
-
-	HOOK(EndScene, VTable[ES]);
-	HOOK(DrawIndexedPrimitive, VTable[DIP]);
-	HOOK(SetStreamSource, VTable[SSS]);
-
+	GetDevice9Methods();
+	HookDevice9Methods();
 	return 0;
 }
 
-BOOL WINAPI DllMain(HMODULE hModule, DWORD dwReason, LPVOID lpvReserved)
+DWORD WINAPI KeyboardHook(void* lParam)
 {
-	if (dwReason == DLL_PROCESS_ATTACH)
+	while (1)
 	{
-		CreateThread(0, 0, MyThread, 0, 0, 0);
+		if (GetAsyncKeyState(VK_NUMPAD0))
+		{
+			StartLog = !StartLog;
+		}
+		if (GetAsyncKeyState(VK_NUMPAD1))
+		{
+			iStride++;
+		}
+		if (GetAsyncKeyState(VK_NUMPAD2))
+		{
+			iStride--;
+		}
+		if (GetAsyncKeyState(VK_NUMPAD4))
+		{
+			iBaseTex++;
+		}
+		if (GetAsyncKeyState(VK_NUMPAD5))
+		{
+			iBaseTex--;
+		}
+		Sleep(100);
 	}
+	return 0;
+}
 
-	return TRUE;
+int WINAPI DllMain(HINSTANCE hInst, DWORD ul_reason_for_call, void* lpReserver)
+{
+	switch (ul_reason_for_call)
+	{
+	case DLL_PROCESS_ATTACH:
+		CreateThread(0, 0, &TF, 0, 0, 0);
+		CreateThread(0, 0, &KeyboardHook, 0, 0, 0);
+	}
+	return 1;
 }
